a66737dd84987f71ce85fbe3bd900b55ae5ffa51
Erik <erik.holmstrom93@gmail.com>
2022-05-07 22:14:12 +0200

    first build

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='faktura-qr-code-reader',
-    version='0.1',
-    scripts=['QrCodeReader'],
+    version='0.0.1',
     author="Erik Holmström",
     author_email="erik.holmstrom93@gmail.com",
-    description="A package for reading qr codes on Swedish invoices",
+    description="Functionality for reading qr codes on Swedish invoices",
+    long_description=long_description,
     url="https://github.com/ErikHolmstrom/qr-code-reader",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+
 )
